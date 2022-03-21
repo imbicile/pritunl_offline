635d7ad5903e1019401c8d634c3bc18f61253cf0
Stanislav Kononov <kononov.s@selectel.ru>
2022-03-21 09:19:53 +0300

    add install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt update
+sudo apt install dkms iptables -y
+sudo dkms autoinstall
+sudo dpkg -i *.deb
+sudo apt install -f -y
+sudo systemctl enable mongod pritunl
+sudo systemctl start mongod pritunl
+sudo dkms autoinstall
+sudo dkms status
+ss -tulpan | grep pritunl
+sleep 5
+sudo pritunl setup-key
